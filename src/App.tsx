8674bc809f15f152fe4856653d886bb9c2d43597
Kyubin Han <kyubinhan11@gmail.com>
2021-02-07 16:16:20 -0800

    Create App component & finish the first iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Chart from "./graphs/Chart";
 
+const width = 800;
+const height = 400;
+const padding = 40;
+
+// TODO: Use React.context to pass down measurements
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Chart width={width} height={height} padding={padding} />;
 }
 
 export default App;
